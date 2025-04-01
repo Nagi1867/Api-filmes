21aecd16e1a76dc45c731bbaf7d6eb868ee45cbf
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-01 17:38:01 -0300

    service, findAll, findById

--- src/main/java/com/example/ApiFilmes/resources/FilmeResource.java
+++ src/main/java/com/example/ApiFilmes/resources/FilmeResource.java
@@ -1,8 +1,11 @@
 package com.example.ApiFilmes.resources;
 
 import com.example.ApiFilmes.entities.Filmes;
+import com.example.ApiFilmes.services.FilmeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,10 +14,18 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "/filmes")
 public class FilmeResource {
-    
+    @Autowired
+    private FilmeService service;
+
     @GetMapping
-    public ResponseEntity<Filmes> findAll(){
-        Filmes filmes = new Filmes(1L, "O gato de botas", "Um gato de botas", "Aventura, Fantasia", "Jorge Lucas", false);
-        return ResponseEntity.ok().body(filmes);
+    public ResponseEntity<List<Filmes>> findAll(){
+        List<Filmes> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Filmes> findById(@PathVariable Long id) {
+        Filmes obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 }
---
+++ src/main/java/com/example/ApiFilmes/services/FilmeService.java
@@ -0,0 +1,24 @@
+package com.example.ApiFilmes.services;
+
+import com.example.ApiFilmes.entities.Filmes;
+import com.example.ApiFilmes.repositories.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FilmeService {
+    @Autowired
+    private FilmeRepository repository;
+
+    public List<Filmes> findAll() {
+        return repository.findAll();
+    }
+
+    public Filmes findById(Long id) {
+        Optional<Filmes> obj = repository.findById(id);
+        return obj.get();
+    }
+}
