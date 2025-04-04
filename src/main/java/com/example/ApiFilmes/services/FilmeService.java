c3293752093c07fcf45ecc2fba9c18930265faa0
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-04-04 16:59:45 -0300

    delete

--- src/main/java/com/example/ApiFilmes/services/FilmeService.java
+++ src/main/java/com/example/ApiFilmes/services/FilmeService.java
@@ -25,4 +25,8 @@ public class FilmeService {
     public Filmes insert(Filmes obj) {
         return repository.save(obj);
     }
+
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
